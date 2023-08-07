1f9e01a6c8a42d8e76ff91445c74430f4aaaeef8
muhammadnawaz110 <131872089+muhammadnawaz110@users.noreply.github.com>
2023-08-07 17:40:03 +0500

    nave bar

--- index.js
+++ index.js
@@ -1,6 +1,13 @@
 const express = require('express')
+const mongoose = require('mongoose')
 
 const app = express()
+
+mongoose.connect('mongodb://127.0.0.1/Blog').then(() =>{
+    console.log('connect tp mongoDb')
+}).catch(error => {
+    console.log(error)
+})
 
 app.listen(5000, () => {
     console.log('App is listing at 5000')
