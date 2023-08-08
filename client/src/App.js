ac58f76d7a85e27680ed065dfc6beed86c0b2172
muhammadnawaz110 <131872089+muhammadnawaz110@users.noreply.github.com>
2023-08-08 18:56:29 +0500

    herosection completed

--- client/src/App.js
+++ client/src/App.js
@@ -1,11 +1,16 @@
 import logo from './logo.svg';
 import './App.css';
 import NaveBar from './components/library/NaveBar';
+import Home from './components/home/Home';
+import { Container } from '@mui/material';
 
 function App() {
   return (
     <div className="App">
         <NaveBar />
+        <Container>
+          <Home />    
+        </Container>
     </div>
   );
 }
---
+++ client/src/components/sections/hero/heroSectionStyle.js
@@ -0,0 +1,43 @@
+export const heroSectionStyle = {
+    twitterIcon: {
+        color: '#fff',
+        backgroundColor: '#1da1f2',
+        fontSize: '1rem',
+        padding: '10px',
+        borderRadius: '50%',
+        marginRight: '0.5rem'
+        
+    },
+
+    facebookIcon: {
+        color: '#fff',
+        backgroundColor: '#0b4c8c',
+        fontSize: '1rem',
+        padding: '10px',
+        borderRadius: '50%',
+        marginRight: '0.5rem'
+        
+    },
+
+    
+    linkedinIcon: {
+        color: '#fff',
+        backgroundColor: '#0b4c8c',
+        fontSize: '1rem',
+        padding: '10px',
+        borderRadius: '50%',
+        marginRight: '0.5rem'
+        
+    },
+
+    
+    youtubeIcon: {
+        color: '#fff',
+        backgroundColor: 'red',
+        fontSize: '1rem',
+        padding: '10px',
+        borderRadius: '50%',
+        marginRight: '0.5rem'
+        
+    }
+}
